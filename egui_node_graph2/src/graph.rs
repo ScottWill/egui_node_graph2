35b43397252fa47ed5a6d2d3f3794e3305f5f649
Scott Will <scottdwill@gmail.com>
2025-11-26 14:25:06 -0600

    Dont show inline for ConnectionOnly input kinds

--- egui_node_graph2/src/graph.rs
+++ egui_node_graph2/src/graph.rs
@@ -20,7 +20,7 @@ pub struct Node<NodeData> {
 
 /// The three kinds of input params. These describe how the graph must behave
 /// with respect to inline widgets and connections for this parameter.
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
 #[cfg_attr(feature = "persistence", derive(Serialize, Deserialize))]
 pub enum InputParamKind {
     /// No constant value can be set. Only incoming connections can produce it
